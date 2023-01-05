5a6729841bbdbc0f24826410eb8a5daa76ffe969
Rizky Ade Satriya <20102158@ittelkom-pwt.ac.id>
2023-01-06 01:17:43 +0700

    first commit

---
+++ Booking_resto_france/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Welcome To Resto .... " << endl;
+    cout << "Please Register for booking " << endl;
+    cout << "Insert biodata " << endl;
+    cout << "Name   : " << endl;
+    cout << "Email  : " << endl;
+    cout << "no tlp : " << endl;
+
+    return 0;
+}
